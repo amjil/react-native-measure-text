7cc57ec75b22bbcc9ddf558e140402efc383ff73
amjil <amjilt@aliyun.com>
2021-12-01 23:05:58 +0800

    fix useCharsWidth option issue & format code

--- ios/MeasureText.m
+++ ios/MeasureText.m
@@ -18,14 +18,14 @@ static NSString *const E_INVALID_TEXTSTYLE = @"E_INVALID_TEXTSTYLE";
 static NSString *const E_INVALID_FONTFAMILY = @"E_INVALID_FONTFAMILY";
 
 static inline BOOL isNull(id str) {
-  return !str || str == (id) kCFNull;
+    return !str || str == (id) kCFNull;
 }
 
 static inline CGFloat CGFloatValueFrom(NSNumber * _Nullable num) {
 #if CGFLOAT_IS_DOUBLE
-  return num ? num.doubleValue : NAN;
+    return num ? num.doubleValue : NAN;
 #else
-  return num ? num.floatValue : NAN;
+    return num ? num.floatValue : NAN;
 #endif
 }
 
@@ -42,11 +42,11 @@ RCT_EXPORT_MODULE()
 // Because the exported constants
 + (BOOL)requiresMainQueueSetup
 {
-  return YES;
+    return YES;
 }
 
 - (dispatch_queue_t)methodQueue {
-  return dispatch_get_main_queue();
+    return dispatch_get_main_queue();
 }
 
 // Example method
@@ -56,94 +56,97 @@ RCT_REMAP_METHOD(multiply,
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 {
-  NSNumber *result = @([a floatValue] * [b floatValue]);
-
-  resolve(result);
+    NSNumber *result = @([a floatValue] * [b floatValue]);
+    
+    resolve(result);
 }
 
 RCT_EXPORT_METHOD(measure:(NSDictionary * _Nullable)options
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
-  // RCTConvert will return nil if the `options` object was not received.
-  NSString *const _Nullable text = [RCTConvert NSString:options[@"text"]];
-  if (isNull(text)) {
-    reject(E_MISSING_TEXT, @"Missing required text.", nil);
-    return;
-  }
-
-  // Allow empty text without generating error
-  // ~~TODO~~: Return the same height as RN. @completed(v2.0.1)
-  if (!text.length) {
-    resolve(@{
-              @"width": @0,
-              @"height": @14,
-              @"lastLineWidth": @0,
-              @"lineCount": @0,
-              });
-    return;
-  }
-
-  // We cann't use RCTConvert since it does not handle font scaling and RN
-  // does not scale the font if a custom delegate has been defined to create.
-  UIFont *const _Nullable font = [self scaledUIFontFromUserSpecs:options];
-  if (!font) {
-    reject(E_INVALID_FONT_SPEC, @"Invalid font specification.", nil);
-    return;
-  }
-
-  // Allow the user to specify the width or height (both optionals).
-  const CGFloat optWidth = CGFloatValueFrom(options[@"width"]);
-  const CGFloat maxWidth = isnan(optWidth) || isinf(optWidth) ? CGFLOAT_MAX : optWidth;
-  const CGSize maxSize = CGSizeMake(maxWidth, CGFLOAT_MAX);
-
-  // Create attributes for the font and the optional letter spacing.
-  const CGFloat letterSpacing = CGFloatValueFrom(options[@"letterSpacing"]);
-  NSDictionary<NSAttributedStringKey,id> *const attributes = isnan(letterSpacing)
-  ? @{NSFontAttributeName: font}
-  : @{NSFontAttributeName: font, NSKernAttributeName: @(letterSpacing)};
-
-  NSTextContainer *textContainer = [[NSTextContainer alloc] initWithSize:maxSize];
-  textContainer.lineFragmentPadding = 0.0;
-  textContainer.lineBreakMode = NSLineBreakByClipping; // no maxlines support
-
-  NSLayoutManager *layoutManager = [NSLayoutManager new];
-  [layoutManager addTextContainer:textContainer];
-  layoutManager.allowsNonContiguousLayout = YES;      // 'cause lastLineWidth
-
-  NSTextStorage *textStorage = [[NSTextStorage alloc] initWithString:text attributes:attributes];
-  [textStorage addLayoutManager:layoutManager];
-
-  [layoutManager ensureLayoutForTextContainer:textContainer];
-  CGSize size = [layoutManager usedRectForTextContainer:textContainer].size;
-  if (!isnan(letterSpacing) && letterSpacing < 0) {
-    size.width -= letterSpacing;
-  }
-
-  const CGFloat epsilon = 0.001;
-  const CGFloat width = MIN(RCTCeilPixelValue(size.width + epsilon), maxSize.width);
-  const CGFloat height = MIN(RCTCeilPixelValue(size.height + epsilon), maxSize.height);
-  const NSInteger lineCount = [self getLineCount:layoutManager];
-
-  NSMutableDictionary *result = [[NSMutableDictionary alloc]
-                                 initWithObjectsAndKeys:@(width), @"width",
-                                 @(height), @"height",
-                                 @(lineCount), @"lineCount",
-                                 nil];
-
-  if ([options[@"usePreciseWidth"] boolValue]) {
-    const CGFloat lastIndex = layoutManager.numberOfGlyphs - 1;
-    const CGSize lastSize = [layoutManager lineFragmentUsedRectForGlyphAtIndex:lastIndex
-                                                                effectiveRange:nil].size;
-    [result setValue:@(lastSize.width) forKey:@"lastLineWidth"];
-  }
-
-    NSMutableArray<NSNumber *> *lineInfo = [self getLineInfo:layoutManager in:textContainer str:text lineNo:lineCount];
-  if (lineInfo) {
-    [result setValue:lineInfo forKey:@"lineInfo"];
-  }
-
-  resolve(result);
+    // RCTConvert will return nil if the `options` object was not received.
+    NSString *const _Nullable text = [RCTConvert NSString:options[@"text"]];
+    if (isNull(text)) {
+        reject(E_MISSING_TEXT, @"Missing required text.", nil);
+        return;
+    }
+    
+    // Allow empty text without generating error
+    // ~~TODO~~: Return the same height as RN. @completed(v2.0.1)
+    if (!text.length) {
+        resolve(@{
+            @"width": @0,
+            @"height": @14,
+            @"lastLineWidth": @0,
+            @"lineCount": @0,
+                });
+        return;
+    }
+    
+    // We cann't use RCTConvert since it does not handle font scaling and RN
+    // does not scale the font if a custom delegate has been defined to create.
+    UIFont *const _Nullable font = [self scaledUIFontFromUserSpecs:options];
+    if (!font) {
+        reject(E_INVALID_FONT_SPEC, @"Invalid font specification.", nil);
+        return;
+    }
+    
+    // Allow the user to specify the width or height (both optionals).
+    const CGFloat optWidth = CGFloatValueFrom(options[@"width"]);
+    const CGFloat maxWidth = isnan(optWidth) || isinf(optWidth) ? CGFLOAT_MAX : optWidth;
+    const CGSize maxSize = CGSizeMake(maxWidth, CGFLOAT_MAX);
+    
+    // Create attributes for the font and the optional letter spacing.
+    const CGFloat letterSpacing = CGFloatValueFrom(options[@"letterSpacing"]);
+    NSDictionary<NSAttributedStringKey,id> *const attributes = isnan(letterSpacing)
+    ? @{NSFontAttributeName: font}
+    : @{NSFontAttributeName: font, NSKernAttributeName: @(letterSpacing)};
+    
+    NSTextContainer *textContainer = [[NSTextContainer alloc] initWithSize:maxSize];
+    textContainer.lineFragmentPadding = 0.0;
+    textContainer.lineBreakMode = NSLineBreakByClipping; // no maxlines support
+    
+    NSLayoutManager *layoutManager = [NSLayoutManager new];
+    [layoutManager addTextContainer:textContainer];
+    layoutManager.allowsNonContiguousLayout = YES;      // 'cause lastLineWidth
+    
+    NSTextStorage *textStorage = [[NSTextStorage alloc] initWithString:text attributes:attributes];
+    [textStorage addLayoutManager:layoutManager];
+    
+    [layoutManager ensureLayoutForTextContainer:textContainer];
+    CGSize size = [layoutManager usedRectForTextContainer:textContainer].size;
+    if (!isnan(letterSpacing) && letterSpacing < 0) {
+        size.width -= letterSpacing;
+    }
+    
+    const CGFloat epsilon = 0.001;
+    const CGFloat width = MIN(RCTCeilPixelValue(size.width + epsilon), maxSize.width);
+    const CGFloat height = MIN(RCTCeilPixelValue(size.height + epsilon), maxSize.height);
+    const NSInteger lineCount = [self getLineCount:layoutManager];
+    
+    NSMutableDictionary *result = [[NSMutableDictionary alloc]
+                                   initWithObjectsAndKeys:@(width), @"width",
+                                   @(height), @"height",
+                                   @(lineCount), @"lineCount",
+                                   nil];
+    
+    if ([options[@"usePreciseWidth"] boolValue]) {
+        const CGFloat lastIndex = layoutManager.numberOfGlyphs - 1;
+        const CGSize lastSize = [layoutManager lineFragmentUsedRectForGlyphAtIndex:lastIndex
+                                                                    effectiveRange:nil].size;
+        [result setValue:@(lastSize.width) forKey:@"lastLineWidth"];
+    }
+    BOOL useCharsWidth = NO;
+    if ([options[@"usePreciseWidth"] boolValue]) {
+        useCharsWidth = YES;
+    }
+    NSMutableArray<NSNumber *> *lineInfo = [self getLineInfo:layoutManager in:textContainer str:text lineNo:lineCount useCharsWidth:useCharsWidth];
+    if (lineInfo) {
+        [result setValue:lineInfo forKey:@"lineInfo"];
+    }
+    
+    resolve(result);
 }
 //
 // ============================================================================
@@ -156,69 +159,69 @@ RCT_EXPORT_METHOD(measure:(NSDictionary * _Nullable)options
  * @since v2.1.0
  */
 - (NSInteger)getLineCount:(NSLayoutManager *)layoutManager {
-  NSRange lineRange;
-  NSUInteger glyphCount = layoutManager.numberOfGlyphs;
-  NSInteger lineCount = 0;
-
-  for (NSUInteger index = 0; index < glyphCount; lineCount++) {
-    [layoutManager
-     lineFragmentUsedRectForGlyphAtIndex:index effectiveRange:&lineRange withoutAdditionalLayout:YES];
-    index = NSMaxRange(lineRange);
-  }
-
-  return lineCount;
+    NSRange lineRange;
+    NSUInteger glyphCount = layoutManager.numberOfGlyphs;
+    NSInteger lineCount = 0;
+    
+    for (NSUInteger index = 0; index < glyphCount; lineCount++) {
+        [layoutManager
+         lineFragmentUsedRectForGlyphAtIndex:index effectiveRange:&lineRange withoutAdditionalLayout:YES];
+        index = NSMaxRange(lineRange);
+    }
+    
+    return lineCount;
 }
 
 /**
  * Get extended info for a given line number.
  * @since v2.1.0
  */
-- (NSMutableArray *)getLineInfo:(NSLayoutManager *)layoutManager in:(NSTextContainer *)textContainer str:(NSString *)str lineNo:(NSInteger)lineTotal {
-  NSMutableArray<NSNumber *> *lineInfo = [[NSMutableArray alloc] initWithCapacity:lineTotal];
-  CGRect lineRect = CGRectZero;
-  NSRange lineRange;
-  NSUInteger glyphCount = layoutManager.numberOfGlyphs;
-  NSInteger lineCount = 0;
-
-
-  for (NSUInteger index = 0; index < glyphCount; lineCount++) {
-    lineRect = [layoutManager
-                lineFragmentUsedRectForGlyphAtIndex:index
-                effectiveRange:&lineRange
-                withoutAdditionalLayout:YES];
-    index = NSMaxRange(lineRange);
-
-    NSCharacterSet *ws = NSCharacterSet.whitespaceAndNewlineCharacterSet;
-    NSRange charRange = [layoutManager characterRangeForGlyphRange:lineRange actualGlyphRange:nil];
-    NSUInteger start = charRange.location;
-    index = NSMaxRange(charRange);
-    /*
-      Get the trimmed range of chars for the glyph range, to be consistent
-      w/android, but the width here will include the trailing whitespace.
-    */
-    if ([options[@"useCharsWidth"] boolValue]) {
-      NSMutableArray<NSNumber *> *charWidths = [[NSMutableArray alloc] initWithCapacity:(index - start)];
-      /*while (index > start && [ws characterIsMember:[str characterAtIndex:index - 1]]) {
-        index--;
-      }*/
-      for (NSUInteger j = 0; j < (index - start); j++) {
-          CGRect boundingRect = [layoutManager boundingRectForGlyphRange:NSMakeRange(lineRange.location + j, 1) inTextContainer:textContainer];
-          const CGFloat boundingWidth = boundingRect.size.width;
-          charWidths[j] = @(boundingWidth);
-      }
-      NSDictionary *line =   @{
-                                 @"line": @(lineCount),
-                                 @"start": @(start),
-                                 @"end": @(index),
-                                 @"width": @(lineRect.size.width),
-                                 @"charWidths": charWidths
-                                 };
-
-      lineInfo[lineCount] = line;
+- (NSMutableArray *)getLineInfo:(NSLayoutManager *)layoutManager in:(NSTextContainer *)textContainer str:(NSString *)str lineNo:(NSInteger)lineTotal useCharsWidth:(BOOL)useCharsWidth {
+    NSMutableArray<NSNumber *> *lineInfo = [[NSMutableArray alloc] initWithCapacity:lineTotal];
+    CGRect lineRect = CGRectZero;
+    NSRange lineRange;
+    NSUInteger glyphCount = layoutManager.numberOfGlyphs;
+    NSInteger lineCount = 0;
+    
+    
+    for (NSUInteger index = 0; index < glyphCount; lineCount++) {
+        lineRect = [layoutManager
+                    lineFragmentUsedRectForGlyphAtIndex:index
+                    effectiveRange:&lineRange
+                    withoutAdditionalLayout:YES];
+        index = NSMaxRange(lineRange);
+        
+        NSCharacterSet *ws = NSCharacterSet.whitespaceAndNewlineCharacterSet;
+        NSRange charRange = [layoutManager characterRangeForGlyphRange:lineRange actualGlyphRange:nil];
+        NSUInteger start = charRange.location;
+        index = NSMaxRange(charRange);
+        /*
+         Get the trimmed range of chars for the glyph range, to be consistent
+         w/android, but the width here will include the trailing whitespace.
+         */
+        if (useCharsWidth == YES) {
+            NSMutableArray<NSNumber *> *charWidths = [[NSMutableArray alloc] initWithCapacity:(index - start)];
+            /*while (index > start && [ws characterIsMember:[str characterAtIndex:index - 1]]) {
+             index--;
+             }*/
+            for (NSUInteger j = 0; j < (index - start); j++) {
+                CGRect boundingRect = [layoutManager boundingRectForGlyphRange:NSMakeRange(lineRange.location + j, 1) inTextContainer:textContainer];
+                const CGFloat boundingWidth = boundingRect.size.width;
+                charWidths[j] = @(boundingWidth);
+            }
+            NSDictionary *line =   @{
+                @"line": @(lineCount),
+                @"start": @(start),
+                @"end": @(index),
+                @"width": @(lineRect.size.width),
+                @"charWidths": charWidths
+            };
+            
+            lineInfo[lineCount] = line;
+        }
     }
-  }
-
-  return lineInfo;
+    
+    return lineInfo;
 }
 
 /**
@@ -230,12 +233,12 @@ RCT_EXPORT_METHOD(measure:(NSDictionary * _Nullable)options
  */
 - (UIFont * _Nullable)scaledUIFontFromUserSpecs:(const NSDictionary *)specs
 {
-  const id allowFontScalingSrc = specs[@"allowFontScaling"];
-  const BOOL allowFontScaling = allowFontScalingSrc ? [allowFontScalingSrc boolValue] : YES;
-  const CGFloat scaleMultiplier =
-  allowFontScaling && _bridge ? _bridge.accessibilityManager.multiplier : 1.0;
-
-  return [self UIFontFromUserSpecs:specs withScale:scaleMultiplier];
+    const id allowFontScalingSrc = specs[@"allowFontScaling"];
+    const BOOL allowFontScaling = allowFontScalingSrc ? [allowFontScalingSrc boolValue] : YES;
+    const CGFloat scaleMultiplier =
+    allowFontScaling && _bridge ? _bridge.accessibilityManager.multiplier : 1.0;
+    
+    return [self UIFontFromUserSpecs:specs withScale:scaleMultiplier];
 }
 
 /**
@@ -244,13 +247,13 @@ RCT_EXPORT_METHOD(measure:(NSDictionary * _Nullable)options
 - (UIFont * _Nullable)UIFontFromUserSpecs:(const NSDictionary *)specs
                                 withScale:(CGFloat)scaleMultiplier
 {
-  return [RCTFont updateFont:nil
-                  withFamily:[RCTConvert NSString:specs[@"fontFamily"]]
-                        size:[RCTConvert NSNumber:specs[@"fontSize"]]
-                      weight:[RCTConvert NSString:specs[@"fontWeight"]]
-                       style:[RCTConvert NSString:specs[@"fontStyle"]]
-                     variant:[RCTConvert NSStringArray:specs[@"fontVariant"]]
-             scaleMultiplier:scaleMultiplier];
+    return [RCTFont updateFont:nil
+                    withFamily:[RCTConvert NSString:specs[@"fontFamily"]]
+                          size:[RCTConvert NSNumber:specs[@"fontSize"]]
+                        weight:[RCTConvert NSString:specs[@"fontWeight"]]
+                         style:[RCTConvert NSString:specs[@"fontStyle"]]
+                       variant:[RCTConvert NSStringArray:specs[@"fontVariant"]]
+               scaleMultiplier:scaleMultiplier];
 }
 
 
